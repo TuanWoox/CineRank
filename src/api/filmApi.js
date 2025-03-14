ec64a714fedf9d01deb414c5e95b7d0639717589
TuanWoox <z1venttt@gmail.com>
2025-03-15 00:00:27 +0700

    Modify to use env key

--- src/api/filmApi.js
+++ src/api/filmApi.js
@@ -1,18 +1,17 @@
+const API_URL = import.meta.env.VITE_URL;
+
 export async function getFilm(submittedSearch) {
   if (!submittedSearch) return []; // Prevent API call with empty search
-  const result = await fetch(
-    `http://www.omdbapi.com/?apikey=c7d3d788&s=${submittedSearch}`
-  );
+  const result = await fetch(`${API_URL}&s=${submittedSearch}`);
   if (!result.ok) {
     throw new Error(`HTTP error! Status: ${result.status}`);
   }
   const data = await result.json();
   return data.Search || []; // Return empty array if no results
 }
+
 export async function getOneFilm(imdbId) {
-  const result = await fetch(
-    `http://www.omdbapi.com/?apikey=c7d3d788&i=${imdbId}`
-  );
+  const result = await fetch(`${API_URL}&i=${imdbId}`);
   const data = await result.json();
   return data.Response === "False" ? null : data;
 }
